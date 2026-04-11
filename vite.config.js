d2be68d26694889526af807af429ab7c0b53d8b2
rashaabdulrazzak <rasha.abdulrazzak@gmail.com>
2026-04-12 01:49:05 +0300

    build

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/portfoloio/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, '.'),
